36ee2e892e1db3ebb5926999b502bc242ddfebc6
IsmailAvayil <ismailavayil@gmail.com>
2023-06-27 00:53:06 +0530

    Updates

---
+++ precache-manifest.8209fa020c080e16b6d2da0890bbd4bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6697b2282d389020f020",
+    "url": "/React-Website/static/css/main.5b0d0fa2.chunk.css"
+  },
+  {
+    "revision": "6697b2282d389020f020",
+    "url": "/React-Website/static/js/main.6697b228.chunk.js"
+  },
+  {
+    "revision": "5c219f066f04fd4786be",
+    "url": "/React-Website/static/js/1.5c219f06.chunk.js"
+  },
+  {
+    "revision": "67dfffd57a02f5504095",
+    "url": "/React-Website/static/js/runtime~main.67dfffd5.js"
+  },
+  {
+    "revision": "84bfb14f1b0ae59d31cc9ed9193be3b9",
+    "url": "/React-Website/index.html"
+  }
+];
